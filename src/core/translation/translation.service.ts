0c0a94dfa8a3b7e7ec5b4a5fefe5c2a7d81a467a
Felix Manuel Martinez Sosa <felixmanuel396@gmail.com>
2024-01-25 22:24:29 -0400

    1

--- src/core/translation/translation.service.ts
+++ src/core/translation/translation.service.ts
@@ -1,35 +1,43 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import * as fs from 'fs';
 import * as path from 'path';
 
 @Injectable()
 export class TranslationService {
   private i18n: Record<string, string> = {};
+  private logger = new Logger('I18n Support');
   constructor(@Inject('DEFAULT_LANGUAGE') private defaultLanguage: string) {
     this.i18n = this.loadLanguageFiles();
-    //console.log(defaultLanguage, this.i18n['exceptions.test']);
+    this.logger.log(`All language files for [${defaultLanguage}] loaded.`);
   }
 
   private loadLanguageFiles(language?: string): Record<string, string> {
     const _lang = language || this.defaultLanguage;
-    return fs
-      .readdirSync(path.join(__dirname, `./i18n/${_lang}`))
-      .filter((file) => file.endsWith('.json'))
-      .map((file) => {
-        const fileName = file.replace('.json', '');
-        // eslint-disable-next-line @typescript-eslint/no-var-requires
-        const fileContent = require(
-          path.join(__dirname, `./i18n/${_lang}/${file}`),
-        );
-        const entries = Object.entries(fileContent).map(([key, value]) => [
-          `${fileName}.${key}`,
-          value,
-        ]);
+    try {
+      return fs
+        .readdirSync(path.join(__dirname, `./i18n/${_lang}`))
+        .filter((file) => file.endsWith('.json'))
+        .map((file) => {
+          const fileName = file.replace('.json', '');
+          // eslint-disable-next-line @typescript-eslint/no-var-requires
+          const fileContent = require(
+            path.join(__dirname, `./i18n/${_lang}/${file}`),
+          );
+          const entries = Object.entries(fileContent).map(([key, value]) => [
+            `${fileName}.${key}`,
+            value,
+          ]);
 
-        return entries;
-      })
-      .reduce((acc, entries) => {
-        return { ...acc, ...Object.fromEntries(entries) };
-      }, {});
+          this.logger.log(``);
+
+          return entries;
+        })
+        .reduce((acc, entries) => {
+          return { ...acc, ...Object.fromEntries(entries) };
+        }, {});
+    } catch (e) {
+      this.logger.error(`Failed to load language file for ${_lang}`);
+      return {};
+    }
   }
 }
